4434885f5222163ae3ddde2def9924c6d48303d6
avalanche42 <81048319+avalanche42@users.noreply.github.com>
2024-05-07 22:14:24 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from './my-recipe-app';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+/*import HomePage from './my-recipe-app';
 import SignupPage from './my-recipe-app';
 import LoginPage from './my-recipe-app';
-import RecipeListPage from './my-recipe-app';
+import RecipeListPage from './my-recipe-app';*/
 
 
 const App = () => {
     return (
         <Router>
-            <Switch>
-                <Route path="/" exact component={HomePage} />
-                <Route path="/signup" component={SignupPage} />
-                <Route path="/login" component={LoginPage} />
-                <Route path="/recipes" component={RecipeListPage} />
-            </Switch>
+            <Routes>
+                <Route path="/" exact component={/my-recipe-app/HomePage} />
+                <Route path="/signup" component={/my-recipe-app/SignupPage} />
+                <Route path="/login" component={/my-recipe-app/LoginPage} />
+                <Route path="/recipes" component={/my-recipe-app/RecipeListPage} />
+            </Routes>
         </Router>
     );
 };
